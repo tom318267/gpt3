6db340a03f14425fad8fe885a8c54a6ff3f82216
tom318267 <ogar318267@gmail.com>
2022-12-19 15:44:00 -0500

    add navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,36 @@
 import React from "react";
 import "../navbar/navbar.scss";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <div className="gpt3__navbar">
-      <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo">
+    <div className="navbar">
+      <div className="navbar__navbar-links">
+        <div className="navbar__logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="navbar__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#gpt3">What is GPT?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="navbar__sign">
+        <p>Sign In</p>
+        <button>Sign Up</button>
       </div>
     </div>
   );
